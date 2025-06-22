0a32ca6fb103ea415f0984223ce4e01cd075b896
joolienhoolien <julienpecquet@gmail.com>
2025-06-22 15:34:11 -0600

    Added replying to posts on topics. Testing as well. Updated some forms and redirects to facilitate this change. Added new URLs for replying.

--- boards/tests/test_view_new_topic.py
+++ boards/tests/test_view_new_topic.py
@@ -1,69 +1,27 @@
-from django.test import TestCase
-from django.urls import reverse
-from django.urls import resolve
 from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse, resolve
 
 from ..forms import NewTopicForm
-from ..views import home, board_topics, new_topic
 from ..models import Board, Topic, Post
+from ..views import new_topic
 
-# Create your tests here.
-class HomeTests(TestCase):
+
+class LoginRequiredNewTopicTests(TestCase):
     def setUp(self):
-        self.board = Board.objects.create(name='Django', description="Django description")
-        url = reverse('home')
-        self.response = self.client.get(url)
+        Board.objects.create(name="Django", description="Django description")
+        self.url = reverse('new_topic', kwargs={'pk': 1})
+        self.response = self.client.get(self.url)
 
-
-    def test_home_view_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_home_url_resolves_home_view(self):
-        view = resolve('/')
-        self.assertEqual(view.func, home)
-
-    def test_home_view_contains_link_to_topics_page(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
-        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
-
-class BoardTopicsTests(TestCase):
-    def setUp(self):
-        Board.objects.create(name='Django', description="Django description")
-
-    def test_board_topics_view_success_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_board_topics_view_not_found_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 99})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_board_topics_url_resolves_board_topics_view(self):
-        view = resolve('/boards/1/')
-        self.assertEqual(view.func, board_topics)
-
-    def test_board_topics_contains_link_back_to_home(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(board_topics_url)
-        homepage_url = reverse('home')
-        self.assertContains(response, 'href="{0}"'.format(homepage_url))
-
-    def test_board_topics_view_contains_navigation_links(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        homepage_url = reverse('home')
-        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
-
-        response = self.client.get(board_topics_url)
-
-        self.assertContains(response, 'href="{0}"'.format(homepage_url))
-        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
 
 class NewTopicTests(TestCase):
     def setUp(self):
         Board.objects.create(name='Django', description="Django description")
-        User.objects.create_user(username='user', email='email@email.com', password='password')
+        user = User.objects.create_user(username='user', email='email@email.com', password='password')
+        self.client.force_login(user=user) #Check here if authentication or redirecting error occurs like 302
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -114,7 +72,7 @@ class NewTopicTests(TestCase):
     def test_contains_form(self):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.get(url)
-        form = response.context['form'] #form = response.context.get('form') ?
+        form = response.context['form']
         self.assertIsInstance(form, NewTopicForm)
 
     def test_new_topic_invalid_post_data(self):
